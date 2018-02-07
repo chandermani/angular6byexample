07c682ec24637757fe89699ef8c8b94a7775e58b
chandermani <chandermani@outlook.com>
2018-02-07 10:01:40 +0530

    Fixed header component implementation.

--- trainer/src/app/core/header/header.component.ts
+++ trainer/src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+
 
 @Component({
   selector: 'abe-header',
@@ -7,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  private showHistoryLink = true;
+  constructor(private router: Router) {
+    this.router.events
+      .filter(e => e instanceof NavigationEnd)
+      .subscribe((e: NavigationEnd) => {
+        this.showHistoryLink = !e.url.startsWith('/workout');
+      });
+  }
 
   ngOnInit() {
   }
