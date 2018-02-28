8fd27de59ea1ca4893f96d64e5eb6fd302203796
Kevin.Hennessy <marykevinh@gmail.com>
2018-02-28 18:31:34 -0500

    Adding sub and side level navigation

--- trainer/src/app/workout-builder/workout-builder.component.ts
+++ trainer/src/app/workout-builder/workout-builder.component.ts
@@ -5,12 +5,12 @@ import { Component, OnInit } from '@angular/core';
                 <div class="row mt-5">
                   <abe-sub-nav-main></abe-sub-nav-main>
                 </div>
-                <div class="row mt-2">
+                <div class="row mt-3">
                   <div class="col-sm-12">
                     <router-outlet></router-outlet>
                   </div>
                 </div>
-            <div>`
+              <div>`
 })
 export class WorkoutBuilderComponent implements OnInit {
 
