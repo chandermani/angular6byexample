10cbd23451e35d9e4592210aff5dd3fbd617a2b1
chandermani <chandermani@outlook.com>
2018-05-12 11:02:15 +0100

    Changed the dependency registration mechanism to use the new angular 6 style.

--- trainer/src/app/core/core.module.ts
+++ trainer/src/app/core/core.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WorkoutHistoryTrackerService } from './workout-history-tracker.service';
-import { LocalStorageService } from './local-storage.service';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 
@@ -11,7 +9,6 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
   declarations: [HeaderComponent],
-  providers: [WorkoutHistoryTrackerService, LocalStorageService],
   exports: [HeaderComponent]
 })
 export class CoreModule { }
